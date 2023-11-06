64cbc19ddf399a86446adf4a22b2bbf96672e202
dci-student <Terissa>
2023-11-06 09:52:11 +0100

    Pairing

--- Solution/Pairing/Pairing.java
+++ Solution/Pairing/Pairing.java
@@ -12,14 +12,21 @@ public class Pairing {
         }
 
         // List<List<String>> is a data type in Java that represents a list of lists of strings,a list that contains other lists.
-        List<List<String>> pairs = new ArrayList<>();
+        List<List<String>> pairs = new ArrayList<>(); // new arrayList called pairs is created. This list is intended
+        // to store pairs of elements, and each pair is represented as a list of strings (List<String>).
         for (int i = 0; i < women.size(); i++) {
             List<String> pair = new ArrayList<>();
+            //Within the loop, a new ArrayList called pair is created. This pair list is meant to hold a single pair
+            // of elements, where the first element is a woman's name and the second element is a man's name.
             pair.add(women.get(i));
+            // the woman's name at position i in the women list is retrieved using women.get(i), and it is added to
+            // the pair list using the add method.
             pair.add(men.get(i));
             pairs.add(pair);
         }
 
         return pairs;
+        //after loop has been processed, the method returns the pairs list, which contains all the pairs of women and
+        // men in the order they were found in the input lists.
     }
 }
--- Solution/Pairing/main.java
+++ Solution/Pairing/main.java
@@ -16,7 +16,7 @@ public class main {
         men1.add("Rolf");
         men1.add("Guymellen");
 
-        List<List<String>> result1 = Pairing.zipIt(women1, men1);
+        List<List<String>> result1 = Pairing.zipIt(women1, men1); // a new variable result1 declared
         System.out.println(result1);  // Output: [["Terissa", "Derick"], ["Carolyn", "Rolf"], ["Yvette", "Guymellen"]]
 
         List<String> women2 = new ArrayList<>();
@@ -56,3 +56,16 @@ public class main {
         System.out.println(result4);  // Output: [["Ana", "Bob"], ["Amy", "Josh"], ["Lisa", "Tim"]]
     }
 }
+
+//NOTES:
+//List<List<String>> result1: This part of the line declares a new variable named result1.
+// It's of type List<List<String>>, which means it's a list of lists of strings. This variable will store the result
+// of the zipIt method, which will be a list of pairs (each pair containing two strings).
+//Pairing.zipIt(women1, men1): This part of the line calls the zipIt method from the Pairing class with women1 and
+// men1 as its arguments.
+//Pairing is the class that contains the zipIt method. To call a method from another class, you specify the class
+// name followed by a dot and then the method name.
+//zipIt is the method being called, and it expects two arguments, which are women1 and men1. These arguments are being
+//passed to the method to perform the pairing of women and men.
+//So, the result of the zipIt method is assigned to the result1 variable. The result1 variable will now contain a list
+//of pairs created by the zipIt method based on the women1 and men1 lists.
